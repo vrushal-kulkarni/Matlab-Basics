bb33a1dedd77063024ef6c19910f273be434fe8f
Vrushal <kulkarni.v@husky.neu.edu>
2017-11-04 21:54:02 -0400

    very basic matlab commands

---
+++ basics.m
@@ -0,0 +1,34 @@
+a=1;    %doesn't print bcoz of semicolon
+b=2;
+c=a+b   %prints in the command line
+
+cos(pi)
+
+a=[1 2 3 4]     %row matrix
+
+b= [1 2 3;4 5 6;7 8 9]      %3*3 matrix
+
+
+b'   %transpose
+
+k=[3 3.5;3.2 3.6]
+
+
+p=k*inv(k)   %A*inv(A) should give identity matrix
+
+a.^2    %squares every element 
+
+A= [a,a]   %concatenates row wise
+A=[b;b]    %concatenates column wise
+
+A (2,3)   %accessing an element of array
+
+A (1:3,2)  %list elements from 1st 3 rows and second column of A
+
+B=0:10:100   %array with step size of 10
+
+myText="Hello World"
+
+max(B)      %finds max element
+disp("hi")  %display simple text
+
